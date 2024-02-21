a91019533473716a4884a389fee76754e44c18c3
Maarten Peels <maartenpeels@gmail.com>
2024-02-21 13:16:01 +0100

    Add `list_directory` plus docstring

--- core/filemanager.py
+++ core/filemanager.py
@@ -3,27 +3,49 @@ import os
 
 
 class FileManager:
+    """Provides methods for file and directory management."""
+
     @staticmethod
     def get_root():
+        """Returns the root directory of the project."""
         return os.path.join(os.path.dirname(__file__), "..")
 
     @staticmethod
+    def get_path(path):
+        """Returns the full path of a file or directory in the project."""
+        return os.path.join(FileManager.get_root(), path)
+
+    @staticmethod
+    def path_exists(path):
+        """Returns True if the path exists, False otherwise."""
+        return os.path.exists(path)
+
+    @staticmethod
     def create_directory(directory):
+        """Creates a directory if it does not exist."""
         if not os.path.exists(directory):
             os.makedirs(directory)
 
     @staticmethod
     def create_directories(root_directory, directories):
+        """Creates a list of directories in the root directory if they do not exist."""
         for directory in directories:
             directory = os.path.join(root_directory, directory)
             FileManager.create_directory(directory)
 
     @staticmethod
-    def path_exists(path):
-        return os.path.exists(path)
+    def list_directory(directory, ends_with=None):
+        """Returns a list of files in a directory. If ends_with is specified, only files ending with the specified
+        string will be returned."""
+        full_path = os.path.join(FileManager.get_root(), directory)
+        files = os.listdir(full_path)
+        if ends_with:
+            files = [f for f in files if f.endswith(ends_with)]
+        return files
 
     @staticmethod
     def open_file(path, mode="r"):
+        """Opens a file in the specified mode. Returns None if the file does not exist and the mode is "r"."""
         full_path = os.path.join(FileManager.get_root(), path)
 
         if mode == "r" and not FileManager.path_exists(full_path):
@@ -32,7 +54,16 @@ class FileManager:
         return open(full_path, mode)
 
     @staticmethod
+    def remove_file(path):
+        """Removes a file if it exists."""
+        full_path = os.path.join(FileManager.get_root(), path)
+
+        if FileManager.path_exists(full_path):
+            os.remove(full_path)
+
+    @staticmethod
     def load_json_file(path, **kwargs):
+        """Loads a JSON file and returns the data. Returns None if the file does not exist."""
         full_path = os.path.join(FileManager.get_root(), path)
 
         if not FileManager.path_exists(full_path):
@@ -43,6 +74,7 @@ class FileManager:
 
     @staticmethod
     def save_json_file(data, path, **kwargs):
+        """Saves data to a JSON file. If the file does not exist, it will be created."""
         full_path = os.path.join(FileManager.get_root(), path)
 
         with FileManager.open_file(full_path, mode="w") as file:
@@ -50,6 +82,7 @@ class FileManager:
 
     @staticmethod
     def copy_file(src_path, dest_path):
+        """Copies a file from the source path to the destination path."""
         full_src_path = os.path.join(FileManager.get_root(), src_path)
         full_dest_path = os.path.join(FileManager.get_root(), dest_path)
 
